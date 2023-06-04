a1dae98c1e6e154297d779b0b014c414fefd8a0a
perazabg <96875802+perazabg@users.noreply.github.com>
2023-06-03 20:05:38 -0700

    testing DOM manipulation

---
+++ main.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+
+const paragraph = document.createElement('div');
+paragraph.classList.add('paragraph');
+paragraph.textContent = 'Hey I\'m red!';
