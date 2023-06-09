e4ff076b5fac5d73ed7c14e99d0876eb0ed627c4
perazabg <96875802+perazabg@users.noreply.github.com>
2023-06-08 21:29:19 -0700

    added p to the dom

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const container = document.querySelector('#container');
 const content = document.createElement('div');
 content.classList.add('content');
 
-const paragraph = document.createElement('div');
+const paragraph = document.createElement('p');
 paragraph.classList.add('paragraph');
 paragraph.textContent = 'Hey I\'m red!';
